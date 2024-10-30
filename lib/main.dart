588f038e188d6b800af7e9e610a979056acf76df
TheHomelander03 <reyjeffgc0785@gmail.com>
2024-10-29 23:36:57 -0400

    added functionality

--- lib/main.dart
+++ lib/main.dart
@@ -50,6 +50,14 @@ class MyHomePage extends StatelessWidget {
     var appState = context.watch<MyAppState>();
     var pair = appState.current;
 
+    IconData icon;
+    if (appState.favorites.contains(pair)) {
+      icon = Icons.favorite;
+    } else {
+      icon = Icons.favorite_border;
+    }
+
+
     return Scaffold(
       body: Center(
         child: Column(
@@ -58,11 +66,25 @@ class MyHomePage extends StatelessWidget {
             Text('A random idea: VeryCool'),
             BigCard(pair: pair),
             SizedBox(height: 10,),      
-            ElevatedButton(
-              onPressed: () {
-                appState.getNext();
-              },
-              child: Text("Next"),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                ElevatedButton.icon(
+                  onPressed: () {
+                    appState.toggleFavorite();
+                  },
+                  icon: Icon(icon),
+                  label: Text('Like'),
+                ),
+                SizedBox(width: 10),
+
+                ElevatedButton(
+                  onPressed: () {
+                    appState.getNext();
+                  },
+                  child: Text('Next'),
+                ),
+              ],
             ),
           ],
         ),
